496a7205463d5185ffaa49880ad8eefef23541e5
Hubert Grobelny <grobelny.hubert@gmail.com>
2020-02-20 00:01:38 +0100

    add navbar

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
+      FormsModule
    ],
-   schemas: [ CUSTOM_ELEMENTS_SCHEMA],
+   schemas: [
+      CUSTOM_ELEMENTS_SCHEMA
+   ],
    providers: [],
    bootstrap: [
       AppComponent
